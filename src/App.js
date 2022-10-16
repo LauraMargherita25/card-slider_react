1244b576f35354e06c8d136044c462342130427a
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-10-16 22:09:01 +0200

    Add style for inputs in command palette

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
 import React, { Component } from "react";
-import Slider from "./SliderComponent";
+import SliderComponent from "./SliderComponent";
+
 
 class App extends Component {
-
-  render() {
-    const cardsData = [
-      {
-        src: 'https://picsum.photos/id/100/100/100',
-        title: 'Card 0',
-        text: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-      },
-      {
-        src: 'https://picsum.photos/id/100/100/100',
-        title: 'Card 1',
-        text: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-      },
-      {
-        src: 'https://picsum.photos/id/100/100/100',
-        title: 'Card 2',
-        text: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-      },
-      {
-        src: 'https://picsum.photos/id/100/100/100',
-        title: 'Card 3',
-        text: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-      },
-      {
-        src: 'https://picsum.photos/id/100/100/100',
-        title: 'Card 4',
-        text: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
-      }
-    ];
-
-    const handleNewCard = (card) => {
-      cardsData.push(card)
+    state = {
+        backgroundColor: "#ffedd5"
     }
 
-    return (
-      <div className="w-full min-w-fit h-screen flex justify-center items-center">
-        <Slider cardsData={cardsData} handleNewCard={handleNewCard}/>
-      </div>
-    )
-  }
+    handleNewBackgroundColor = (newColor) => {
+        this.setState({
+            backgroundColor: newColor
+        })
+    }
+
+    render() {
+        return(
+            <div className="w-full min-w-fit h-screen flex justify-center items-center" style={{ backgroundColor: this.state.backgroundColor}}>
+
+                <SliderComponent backgroundColor={this.state.backgroundColor} handleNewBackgroundColor={this.handleNewBackgroundColor}/>
+
+            </div>
+        )
+    }
 }
 
 export default App;
